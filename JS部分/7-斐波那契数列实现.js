f8fc1c7de8cc7e389a69c5c0dc4b740b5c4607a5
PeterChen <747853229@qq.com>
2018-02-21 16:21:41 +0800

    更新长度单位小结

---
+++ JS部分/7-斐波那契数列实现.js
@@ -0,0 +1,34 @@
+// 斐波那契而数列： 1,1,2,3,5,8...
+
+// 递归实现
+function Fibonacci(n) {
+  let number = Number(n) === parseInt(n) ? Number(n) : -1
+  console.log(number)
+  if(number <= 0) {
+    throw new Error('Please input a valid number')
+  } else {
+    if(n == 1 || n == 2) {
+      return 1
+    } else {
+      return Fibonacci(n - 1) + Fibonacci(n - 2)
+    }
+  }
+}
+
+// 自底向上求
+function FibonacciII(n) {
+  let number = Number(n) === parseInt(n) ? Number(n) : -1
+  if(number <= 0) {
+    throw new Error('Please')
+  } else {
+    if(n == 1 || n == 2) {
+      return 1
+    } else {
+      let fibArr = [1, 1]
+      for(let i = 2; i < n; i++) {
+        fibArr.push(fibArr[i - 2] + fibArr[i - 1])
+      }
+      return fibArr[n - 1]
+    }
+  }
+}
