4630fe776d1771f69c1e730fe93adf4c0fbe2c82
PeterChen <747853229@qq.com>
2018-02-25 13:32:39 +0800

    剑指Offer

---
+++ 剑指offer/57-二叉树的下一个节点.js
@@ -0,0 +1,34 @@
+// 题目描述：给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回
+
+/*function TreeLinkNode(x){
+    this.val = x;
+    this.left = null;
+    this.right = null;
+    this.next = null;
+}*/
+function GetNext(pNode)
+{
+    if(!pNode) {
+      return null
+    }
+    // 存在右子树，往右边走
+    if(pNode.right) {
+      let p = pNode.right
+      // 寻找右子树的最左结点
+      while(p.left) {
+        p = p.left
+      }
+      return p
+    }
+    // 没有右子树，往上找
+    while(pNode.next) {
+      // 找父节点
+      // 如果为父节点的左结点
+      if(pNode === pNode.next.left) {
+        return pNode.next
+      }
+      // 否则继续往上找
+      pNode = pNode.next
+    }
+    return null
+}
---
+++ 剑指offer/58-对称的二叉树.js
@@ -0,0 +1,28 @@
+// 题目描述：请实现一个函数，用来判断一颗二叉树是不是对称的。注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
+
+/* function TreeNode(x) {
+    this.val = x;
+    this.left = null;
+    this.right = null;
+} */
+function isSymmetricalII(lRoot,rRoot) {
+  if(!lRoot && !rRoot) {
+    return true
+  } else if (!lRoot || !rRoot) {
+    return false
+  } else if (lRoot.val !== rRoot.val) {
+    return false
+  }
+  return isSymmetricalII(lRoot.left,rRoot.right) && isSymmetricalII(lRoot.right,rRoot.left)
+}
+
+function isSymmetrical(pRoot)
+{
+  if(!pRoot) {
+    return true
+  } else if(pRoot.left && !pRoot.right || !pRoot.left && pRoot.right ) {
+    return false
+  }
+  
+  return isSymmetricalII(pRoot.left,pRoot.right)
+}
