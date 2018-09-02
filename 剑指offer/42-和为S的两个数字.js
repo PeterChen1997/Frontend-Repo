a2af0249cdf13474b703b7cab80aad38b0bccd3b
own <own@promote.cache-dns.local>
2018-09-02 10:43:16 +0800

    modify code detail

--- 剑指offer/42-和为S的两个数字.js
+++ 剑指offer/42-和为S的两个数字.js
@@ -1,21 +1,20 @@
 // 输入一个递增排序的数组和一个数字S，在数组中查找两个数，是的他们的和正好是S，如果有多对数字的和等于S，输出两个数的乘积最小的。
 
 // 思路：使用两个指针，头尾开始搜索， 相等开始比较，找小的
-function FindNumbersWithSum(array, sum)
-{
-    if(!array) {
+function FindNumbersWithSum(array, sum) {
+    if (!array) {
         return null
     }
     let aHead = 0
     let bHead = array.length - 1
     let minResult = Number.MAX_SAFE_INTEGER
     let arr = []
-    while(aHead <= bHead) {
+    while (aHead <= bHead) {
         let total = array[aHead] + array[bHead]
-        if(total > sum) {
+        if (total > sum) {
             bHead--
-        } else if(total === sum) {
-            if(total < minResult) {
+        } else if (total === sum) {
+            if (total < minResult) {
                 minResult = total
                 arr.length = 0
                 arr.push(array[aHead])
--- 剑指offer/55-链表中环的入口结点.js
+++ 剑指offer/55-链表中环的入口结点.js
@@ -9,25 +9,24 @@
 // 1. 一个指针走一步，一个指针走两步，如果在到Null之前重合，则证明有环存在
 // 2. 假设这个环走了若干个结点，但是由于要圈上 多走整数圈数 才能相遇，则这个等式成立 2x = x + r*n，r为圈数
 // 3. 那么此时新建一个从头开始，走一步的指针，同时继续走，走x步即可回到相遇点，但是如果在相遇点之前重合，则重合点为入口
-function EntryNodeOfLoop(pHead)
-{
-    if(!pHead || !pHead.next) {
-      return null
+function EntryNodeOfLoop(pHead) {
+  if (!pHead || !pHead.next) {
+    return null
+  }
+  let fast = pHead
+  let slow = pHead
+  // 题目已知有环
+  while (1) {
+    fast = fast.next
+    slow = slow.next.next
+    if (fast === slow) {
+      break
     }
-    let fast = pHead
-    let slow = pHead
-    // 题目已知有环
-    while(1) {
-      fast = fast.next
-      slow = slow.next.next
-      if(fast === slow) {
-        break
-      }
-    }
-    let result = pHead
-    while(result !== slow) {
-      slow = slow.next
-      result = result.next
-    }
-    return result
+  }
+  let result = pHead
+  while (result !== slow) {
+    slow = slow.next
+    result = result.next
+  }
+  return result
 }
