b563472261aa925a56d879d84db6c1968019bc28
Peter Chen <747853229@qq.com>
2020-07-17 22:43:58 +0800

    feat: add leetcode

---
+++ leetcode/困难/273.整数转换英文表示.js
@@ -0,0 +1,30 @@
+/**
+将非负整数转换为其对应的英文表示。可以保证给定输入小于 231 - 1 。
+
+示例 1:
+
+输入: 123
+输出: "One Hundred Twenty Three"
+示例 2:
+
+输入: 12345
+输出: "Twelve Thousand Three Hundred Forty Five"
+示例 3:
+
+输入: 1234567
+输出: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+示例 4:
+
+输入: 1234567891
+输出: "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"
+
+ */
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+const numberToWords = = (num) => {
+    
+
+};
--- leetcode/困难/51.N皇后.js
+++ leetcode/困难/51.N皇后.js
@@ -30,15 +30,89 @@ n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，�
  */
 
 // 思路： 递归 回溯
-// 时间复杂度：
-// 空间复杂度：
+// 难点是怎么用代码描述不能放置的位置
+// 时间复杂度：时间复杂度：o(n!)
+// 空间复杂度：o(n)
+
+function genCheckerBoard(n) {
+    let arr = new Array(n)
+    for (let i = 0; i < n; i++) {
+        arr[i] = new Array(n).fill('.')
+    }
+    return arr
+}
 
 /**
  * @param {number} n
  * @return {string[][]}
  */
 const solveNQueens = (n) => {
+    if (n <= 0) {
+        return
+    } else if (n === 1) {
+        return [['Q']]
+    }
 
+    // 结果数组
+    const ansArr = []
+
+    // 当前棋盘
+    let board = genCheckerBoard(n)
+    // 斜率 1 方向
+    let line1 = new Array(2 * n - 1).fill(false)
+    // 斜率 -1 方向
+    let line2 = new Array(2 * n - 1).fill(false)
+    // 当前列 方向
+    let col = new Array(n).fill(false)
+
+    function isValidPos(x, y, n) {
+        return !col[x] && !line1[x - y + n - 1] && !line2[x + y]
+    }
+
+    function put(x, y, n) {
+        // 放置棋子
+        board[x][y] = 'Q'
+        // 斜率1方向占住位置 (斜向线序号从0开始)
+        line1[x - y + n - 1] = true
+        // 斜率-1方向占住位置 (斜向线序号从0开始)
+        line2[x + y] = true
+        // 占住当前列
+        col[x] = true
+    }
+    function remove(x, y, n) {
+        // 同put
+        board[x][y] = '.'
+        line1[x - y + n - 1] = false
+        line2[x + y] = false
+        col[x] = false
+    }
+
+    function nQueen(n, y) {
+        // 函数出口
+        if (y === n) {
+            ansArr.push(board.map(item => item.join('')))
+            return
+        }
+
+        // 从第0列开始
+        for (let x = 0; x < n; x++) {
+            // 判断能不能放
+            if (!isValidPos(x, y, n)) {
+                continue
+            }
+            // 放置棋子
+            put(x, y, n)
+            // 迭代放置后的情况
+            nQueen(n, y + 1)
+            // 移除当前位置的棋子，从下一列重新开始
+            remove(x, y, n)
+        }
+    }
+
+    // 开始迭代
+    nQueen(n, 0)
+
+    return ansArr
 };
 
-console.log(solveNQueens())
+console.log(solveNQueens(1))
